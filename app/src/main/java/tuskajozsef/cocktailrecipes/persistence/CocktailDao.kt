32fdc1a681a0acf545e1951fdadaa40a9613418b
Tuska József <jozsef.tuska@roksh.com>
2022-04-16 12:13:24 +0200

    added room

--- app/src/main/java/tuskajozsef/cocktailrecipes/persistence/CocktailDao.kt
+++ app/src/main/java/tuskajozsef/cocktailrecipes/persistence/CocktailDao.kt
@@ -1,3 +1,19 @@
 package tuskajozsef.cocktailrecipes.persistence
 
-class CocktailDao(val name: String, val image: String)
+import tuskajozsef.cocktailrecipes.model.Cocktail
+import androidx.room.*;
+
+@Dao
+interface CocktailDao {
+    @Query("SELECT * FROM cocktails")
+    fun getAllCities(): List<Cocktail>
+
+    @Insert
+    fun insertCity(cocktail: Cocktail) : Long
+
+    @Delete
+    fun deleteCity(cocktail: Cocktail)
+
+    @Update
+    fun updateCity(cocktail: Cocktail)
+}
