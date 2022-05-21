f22b015dde7de6d5a5c5e884c3afd2c3a8c070c7
Tuska József <jozsef.tuska@roksh.com>
2022-05-21 11:22:11 +0200

    added google analytics and crash analytics

--- app/src/main/java/tuskajozsef/cocktailrecipes/ui/main/MainActivity.kt
+++ app/src/main/java/tuskajozsef/cocktailrecipes/ui/main/MainActivity.kt
@@ -7,6 +7,10 @@ import dagger.hilt.android.AndroidEntryPoint
 import tuskajozsef.cocktailrecipes.R
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.ktx.logEvent
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -19,10 +23,13 @@ import tuskajozsef.cocktailrecipes.ui.main.adapter.RecyclerViewAdapter
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
     private val viewModel: MainViewModel by viewModels()
     var cocktails: MutableList<Cocktail> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        firebaseAnalytics = Firebase.analytics
+
         setContentView(R.layout.activity_main)
         super.onCreate(savedInstanceState)
 
@@ -37,6 +44,14 @@ class MainActivity : ComponentActivity() {
             .launchIn(CoroutineScope(Dispatchers.IO))
 
         searchButton.setOnClickListener{
+            //throw RuntimeException("Test Crash") // Force a crash
+
+            firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_ITEM) {
+                param(FirebaseAnalytics.Param.ITEM_ID, "id")
+                param(FirebaseAnalytics.Param.ITEM_NAME, "button click")
+                param(FirebaseAnalytics.Param.CONTENT_TYPE, "button")
+            }
+
             val ingredient = searchCocktailIngredient.text?.toString()
 
             if(ingredient != null)
